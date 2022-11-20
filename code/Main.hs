ab975d5077b45f2d6cabbb7d4f26f6dd77a1f9f5
GustavoAPS <gugustavo137@gmail.com>
2022-11-20 16:50:43 -0300

    Adicionando Main

---
+++ code/Main.hs
@@ -0,0 +1,12 @@
+
+player_map = [[1, 0, 0, 0, 0],[1, 0, 0, 0, 0],[0, 0, 0, 1, 1],[0, 0, 0, 0, 0],[0, 0, 0, 0, 0]]
+
+printPlayerMap = do
+    putStr("\n")
+    print(player_map !! 0)
+    print(player_map !! 1)
+    print(player_map !! 2)
+    print(player_map !! 3)
+    print(player_map !! 4)
+    putStr("\n")
+
