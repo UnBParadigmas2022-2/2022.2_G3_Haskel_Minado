effbc6b99dc357699e9834d0eb6424751c03365e
GustavoAPS <gugustavo137@gmail.com>
2022-11-23 17:11:05 -0300

    limpando argumento desnecesario da main e organizando codigo

--- code/Main.hs
+++ code/Main.hs
@@ -1,15 +1,12 @@
 -- representation of the map
 import Data.List
 player_one_map = [[1, 0, 0, 0, 0],[0, 0, 0, 0, 0],[0, 0, 0, 0, 0],[0, 0, 0, 0, 0],[0, 0, 0, 0, 0]]
-player_two_map = [[0, 0, 0, 0, 0],[0, 0, 0, 0, 0],[0, 0, 0, 0, 0],[0, 0, 0, 0, 0],[0, 0, 0, 0, 1]]
+player_two_map = [[1, 0, 0, 0, 0],[0, 0, 0, 0, 0],[0, 0, 0, 0, 0],[0, 0, 0, 0, 0],[0, 0, 0, 0, 0]]
 
 
 -- checking if the all row of the map has one
-checkIfListHasOne = (1 `elem` player_one_map!!0) || 
-                    (1 `elem` player_one_map!!1) ||
-                    (1 `elem` player_one_map!!2) ||
-                    (1 `elem` player_one_map!!3) ||
-                    (1 `elem` player_one_map!!4)
+checkShipsAlive :: [[Int]] -> Bool
+checkShipsAlive mapToCheck = (1 `elem` mapToCheck!!0) || (1 `elem` mapToCheck!!1) || (1 `elem` mapToCheck!!2) || (1 `elem` mapToCheck!!3) || (1 `elem` mapToCheck!!4)
 
 
 -- funcao que modifica um elemento da Matriz
@@ -39,31 +36,45 @@ printMap mapToPrint = do
     putStr("\n")
 
 
-runGame :: [[Int]] -> [[Int]] -> Int -> IO ()
-runGame player_one_map player_two_map points = do
+-- Game recursion function
+runGame :: [[Int]] -> [[Int]] -> IO ()
+runGame player_one_map player_two_map = do
 
-    --if checkIfListHasOne == True
-    if points == 3
-    then putStrLn "Fim de Jogo"
+    if (checkShipsAlive player_two_map) == False || (checkShipsAlive player_one_map) == False
+    then do 
+      putStrLn "Fim de Jogo"
+
+      if (checkShipsAlive player_one_map == False)
+      then do
+        putStrLn "Jogador 2 venceu"
+      else
+        putStrLn "Jogador 1 venceu"
+
     else do
-      putStrLn "Batalha Naval"
+      
       printMap player_one_map
       printMap player_two_map
 
       putStrLn "Jogador 1 - Qual coordenada X para atacar?"
-      x_coord <- getLine
+      player_one_x_coord <- getLine
 
       putStrLn "Jogador 1 - Qual coordenada Y para atacar?"
-      y_coord <- getLine
+      player_one_y_coord <- getLine
 
+      let novoMapaJogador2 = updateMatrix player_two_map 2 ((read player_one_x_coord :: Int),(read player_one_y_coord :: Int))
 
-      let novoMapaJogador2 = updateMatrix player_two_map 2 ((read x_coord :: Int),(read y_coord :: Int))
-      --let resultado = append [0,0] attacked_positions
-      --print( append [1,2] (append [0,0] attacked_positions))
-      
-      runGame player_one_map novoMapaJogador2 (points + 1)
+      putStrLn "Jogador 2 - Qual coordenada X para atacar?"
+      player_two_x_coord <- getLine
+
+      putStrLn "Jogador 2 - Qual coordenada Y para atacar?"
+      player_two_y_coord <- getLine
+
+      let novoMapaJogador1 = updateMatrix player_two_map 2 ((read player_two_x_coord :: Int),(read player_two_y_coord :: Int))
+
+      runGame novoMapaJogador1 novoMapaJogador2
 
 
 main :: IO ()
 main = do
-  runGame player_one_map player_two_map 1
+  putStrLn "- Batalha Naval -"
+  runGame player_one_map player_two_map
