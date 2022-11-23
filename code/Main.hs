1e22b2d1ad7cd3f9558c8762881ab39071fd8ae4
Peh099 <70337717+Peh099@users.noreply.github.com>
2022-11-23 08:12:09 -0300

    Adicionando funcao updateMatrix
    Co-authored-by: LorrayneCardozo <lorraynecardozo15@gmail.com>

--- code/Main.hs
+++ code/Main.hs
@@ -1,4 +1,5 @@
 -- representation of the map
+import Data.List
 player_map = [[1, 0, 0, 0, 0],[1, 0, 0, 0, 0],[0, 0, 0, 1, 1],[0, 0, 0, 0, 0],[0, 0, 0, 0, 0]]
 
 -- print of the map
@@ -17,6 +18,14 @@ checkIfListHasOne = (1 `elem` player_map!!0) ||
                     (1 `elem` player_map!!2) ||
                     (1 `elem` player_map!!3) ||
                     (1 `elem` player_map!!4)
+
+-- funcao que modifica um elemento da Matriz
+--     [Matriz] -> novovalor (x, y)
+updateMatrix :: [[a]] -> a -> (Int, Int) -> [[a]]
+updateMatrix m player_map (r,c) =
+  take r m ++
+  [take c (m !! r) ++ [player_map] ++ drop (c + 1) (m !! r)] ++
+  drop (r + 1) m
 
 -- Appending integer list to list of lists
 append :: [Int] -> [[Int]] -> [[Int]]
