d0cedb706355f49a1bd9f6f82f9a529b48f110c2
Gustave Persijn <gpersijn@outlook.com>
2022-11-23 22:23:16 -0300

    Adding new feature Coordinate
    Co-authored-by: Peh099 <pedrohenriquec099@gmail.com>
    Co-authored-by: LorrayneCardozo <lorraynecardozo15@gmail.com>

--- code/Main.hs
+++ code/Main.hs
@@ -1,22 +1,30 @@
 -- representation of the map
 import Data.List
+import Data.Char (ord)
+
+type Coordinate = (Int, Int)
+
 player_one_map = [[1, 0, 0, 0, 0],[0, 0, 0, 0, 0],[0, 0, 0, 0, 0],[0, 0, 0, 0, 0],[0, 0, 0, 0, 0]]
 player_two_map = [[1, 0, 0, 0, 0],[0, 0, 0, 0, 0],[0, 0, 0, 0, 0],[0, 0, 0, 0, 0],[0, 0, 0, 0, 0]]
-
 
 -- checking if the all row of the map has one
 checkShipsAlive :: [[Int]] -> Bool
 checkShipsAlive mapToCheck = (1 `elem` mapToCheck!!0) || (1 `elem` mapToCheck!!1) || (1 `elem` mapToCheck!!2) || (1 `elem` mapToCheck!!3) || (1 `elem` mapToCheck!!4)
 
-
 -- funcao que modifica um elemento da Matriz
 -- [Matriz] -> novovalor (x, y)
-updateMatrix :: [[a]] -> a -> (Int, Int) -> [[a]]
+updateMatrix :: [[a]] -> a -> Coordinate -> [[a]]
 updateMatrix m player_map (r,c) =
   take r m ++
   [take c (m !! r) ++ [player_map] ++ drop (c + 1) (m !! r)] ++
   drop (r + 1) m
 
+-- Extract the coordinate from the string
+-- Also immediately convert the coordinate from range [0,10[ to [1,10]
+-- An invalid coordinate is returned when the string isn't of the correct style.
+convertStringToCoordinates :: String -> Coordinate
+convertStringToCoordinates ['(', x, ',', y, ')'] = ((ord x) - (ord '0'), (ord y) - (ord '0'))
+convertStringToCoordinates _ = (-1, -1)
 
 -- Appending integer list to list of lists
 append :: [Int] -> [[Int]] -> [[Int]]
@@ -34,7 +42,6 @@ printMap mapToPrint = do
     print(mapToPrint !! 3)
     print(mapToPrint !! 4)
     putStr("\n")
-
 
 -- Game recursion function
 runGame :: [[Int]] -> [[Int]] -> IO ()
@@ -55,24 +62,17 @@ runGame player_one_map player_two_map = do
       printMap player_one_map
       printMap player_two_map
 
-      putStrLn "Jogador 1 - Qual coordenada X para atacar?"
-      player_one_x_coord <- getLine
+      putStrLn ("Jogador 1 - Qual coordenadas para atacar (x,y)?")
+      string <- getLine
+      let coord = convertStringToCoordinates string
+      let novoMapaJogador2 = updateMatrix player_two_map 2 coord
 
-      putStrLn "Jogador 1 - Qual coordenada Y para atacar?"
-      player_one_y_coord <- getLine
-
-      let novoMapaJogador2 = updateMatrix player_two_map 2 ((read player_one_x_coord :: Int),(read player_one_y_coord :: Int))
-
-      putStrLn "Jogador 2 - Qual coordenada X para atacar?"
-      player_two_x_coord <- getLine
-
-      putStrLn "Jogador 2 - Qual coordenada Y para atacar?"
-      player_two_y_coord <- getLine
-
-      let novoMapaJogador1 = updateMatrix player_two_map 2 ((read player_two_x_coord :: Int),(read player_two_y_coord :: Int))
+      putStrLn "Jogador 2 - Qual coordenadas para atacar (x,y)?"
+      string2 <- getLine
+      let coord2 = convertStringToCoordinates string2
+      let novoMapaJogador1 = updateMatrix player_two_map 2 coord2
 
       runGame novoMapaJogador1 novoMapaJogador2
-
 
 main :: IO ()
 main = do
